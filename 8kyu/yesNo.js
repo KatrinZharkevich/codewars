90250a5d66d9ac6d43d6514475600b17b5a549d8
KatrinZharkevich <katerinazharkevich@gmail.com>
2026-08-03 23:24:05 +0300

    yesNo

---
+++ 8kyu/yesNo.js
@@ -0,0 +1,20 @@
+// 😁 DESCRIPTION
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// 🐱‍👤TESTS
+//assert.strictEqual(boolToWord(true), 'Yes')
+// assert.strictEqual(boolToWord(false), 'No')
+
+// 👨‍💻SOLUTION
+
+// MyVariant
+function boolToWord(value){
+    return value === true ?  'Yes' : 'No'
+}
+console.log(boolToWord(true));
+
+// Var2
+// function boolToWord( bool ){
+//     return bool ? 'Yes':'No';
+// }
+
